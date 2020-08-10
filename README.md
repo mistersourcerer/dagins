700d7866f3336c91632dc4cc632352f74d127dc4
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2020-08-10 23:07:26 +0200

    It is alive.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Dagins
 
 ## A command line utility to keep you accountable about your daily habits cultivation
+
+## Should I use it?
+
+Not yet. :)
--- dagins.go
+++ dagins.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Dagins!")
+	version := flag.Bool("version", false, "Show this dagins version.")
+	v := flag.Bool("v", false, "Show this dagins version.")
+	flag.Parse()
+
+	switch {
+	case *version || *v:
+		showVersion()
+	default:
+		dagins()
+	}
+}
+
+func showVersion() {
+	fmt.Println("dagins version 0.0.1a")
+}
+
+func dagins() {
+	fmt.Println("JUST DO IT! STAY ON TARGET! YOURS TRULY, DAGINS!")
 }
