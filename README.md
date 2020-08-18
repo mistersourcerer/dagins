37f8ed34161d742be034fbd0d6cabbd8c1abf25e
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2020-08-18 23:31:32 +0200

    Store (not correctly yet) JSON with goals.

--- README.md
+++ README.md
@@ -21,7 +21,43 @@ Go do your thing and let `dagins` know that you were a trooper about it:
     $ dagins goal done "learn go"
     ➜ Way to go. I will be here for you tomorrow.
 
-
 ## Should I use it?
 
 Not yet. :)
+
+## How it works
+
+Information is stored in two JSON files: goals.json and progress.json.
+
+### goals.json
+
+```json
+{
+  "goals": [
+    {
+      "name": "learn go",
+      "periodicity: "daily"
+    }
+  ]
+}
+```
+
+### progress.json
+
+```json
+{
+  "goals": [
+    {
+      "name": "learn go",
+      "streak": 3,
+      "completions": [
+        "2020-01-01 4:20:00",
+        "2020-01-02 4:20:00",
+        "2020-01-06 4:20:00",
+        "2020-01-07 4:20:00",
+        "2020-01-08 4:20:00",
+      ]
+    }
+  ]
+}
+```
--- goal.go
+++ goal.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 )
+
+type goal struct {
+	Name        string
+	Periodicity string
+}
 
 func HandleGoal(cmd []string) {
 	switch command := cmd[0]; command {
 	case "create":
-		// handle abscence of a name...
-		create(cmd[1])
+		if len(cmd[1:]) < 1 {
+			fmt.Println("goal need a name")
+			fmt.Println("  $ dagins goal create 'read one page'")
+		} else {
+			// extract periodicity
+			create(cmd[1], "daily")
+		}
 	}
 }
 
-func create(name string) {
-	fmt.Println("Creating", name)
+func create(name string, periodicity string) {
+	// validate periodicity
+	fmt.Println("Creating", name, periodicity)
+	newGoal := goal{Name: name, Periodicity: periodicity}
+	save(&newGoal)
+}
+
+func save(newGoal *goal) {
+	// handle error
+	os.Mkdir(".dagins", 0755)
+
+	f, err := os.OpenFile(".dagins/goals.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer f.Close()
+
+	// handle error
+	goalJSON, err := json.Marshal(newGoal)
+
+	// handle error
+	f.Write(goalJSON)
+	f.Write([]byte("\n"))
 }
